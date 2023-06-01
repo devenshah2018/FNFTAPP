69287f8c07ae7844552de9dfd2eeb9256c6e40ed
Deven Shah <devenshah2018@gmail.com>
2023-05-31 23:22:14 -0700

    Added search

--- wwwroot/js/routing.js
+++ wwwroot/js/routing.js
@@ -4,6 +4,5 @@ function redirectToEvent(imageID) {
     var url = "/templates/event-template.html" + "?artist=" + encodeURIComponent(imageID);
 
     window.location.href = url;
-    // getelemendbyid image
-    // display picture of source image
+
 }
--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -11,3 +11,18 @@ scrollButtons.forEach(button => {
         });
     });
 });
+
+function filterImages() {
+    var searchInput = document.querySelector('.search-input');
+    var inputValue = searchInput.value.toLowerCase();
+    var imageContainers = document.querySelectorAll('.image-container');
+
+    imageContainers.forEach(function (container) {
+        var containerClass = container.classList[1];
+        if (containerClass.includes(inputValue)) {
+            container.style.display = 'block';
+        } else {
+            container.style.display = 'none';
+        }
+    });
+}

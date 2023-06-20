78a273e2ee95658afa414f958c4615488c57e573
ovsharam <sharmaapoorva124@gmail.com>
2023-06-20 11:03:39 -0700

    Resolving override for deployer errors

--- Models/Fantom/contracts/ERC1155.sol
+++ Models/Fantom/contracts/ERC1155.sol
@@ -4,12 +4,12 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+// import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+// import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
-contract SFTix is ERC1155, Ownable, ERC1155Burnable, ERC1155Supply {
+contract SFTix is ERC1155, Ownable {
 
     // Create a mapping, isEventOwner
     mapping(address => bool) public isEventCreator;
@@ -56,22 +56,6 @@ contract SFTix is ERC1155, Ownable, ERC1155Burnable, ERC1155Supply {
             mint(to, i + _Array_GA.length, _Array_VIP[i], data);
         }
     }
-    
-
-
-
-
-    function _beforeTokenTransfer(
-        address operator,
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) internal override(ERC1155, ERC1155Supply) {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-    }
-
 
 }
 
